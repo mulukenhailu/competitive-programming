562ebcda5f121fb604e84c78cb30bb01e38ab338
Muluken Hailu <108363098+mulukenhailu@users.noreply.github.com>
2023-12-12 11:58:30 +0300

    Added solution for Sum of Even Numbers After Queries

---
+++ week2/day5/sum-of-even-numbers-after-queries.py
@@ -0,0 +1,35 @@
+class Solution:
+    def sumEvenAfterQueries(self, nums: List[int], queries: List[List[int]]) -> List[int]:
+        # =6
+        # 1234
+        # 2234=8=6+2
+        # 2-134=6=8-2
+        # -2-134=2=6+(-2-2)
+        # -2-136=2+(6-4)=4
+        # -2-156=4
+        
+    
+        ans=[]
+        cur_even_sum=sum([num for num in nums if num%2==0])
+        print(cur_even_sum)
+        
+        for q in queries:
+            idx=q[1]
+            val=q[0]
+            preNum=nums[idx]
+            NewNum=nums[idx]+val
+            if NewNum%2==0 and preNum%2!=0:
+                ans.append(cur_even_sum+NewNum)
+                cur_even_sum+=NewNum
+            elif NewNum%2==0 and preNum%2==0:
+                ans.append(cur_even_sum+(NewNum-preNum))
+                cur_even_sum+=(NewNum-preNum)
+            elif NewNum%2!=0 and preNum%2==0:
+                ans.append(cur_even_sum-preNum)
+                cur_even_sum-=preNum
+            else:
+                ans.append(cur_even_sum)
+            nums[idx]=NewNum
+        return ans
+            
+            
