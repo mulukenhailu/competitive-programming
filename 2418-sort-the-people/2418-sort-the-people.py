bf8c74e33ddf29eec9d9ea364dc496d101a46f99
Muluken Hailu <108363098+mulukenhailu@users.noreply.github.com>
2023-12-26 22:34:05 +0300

    Time: 455 ms (10.99%), Space: 18 MB (6.12%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,14 +1,18 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        dic={}
+        l=len(heights)
+        dic=defaultdict()
         for h, n in zip(heights, names):
             dic[h]=n
-        dic=dict(sorted(dic.items(), key=lambda item : item[0], reverse=True))
-        ans=[]
-        print(dic)
-        for k, v in dic.items():
-            ans.append(v)
-        return ans
+        
+        for i in range(l):
+            for j in range(i+1, l):
+                if heights[i]>heights[j]:
+                    heights[i], heights[j]=heights[j], heights[i]
+    
+        return [dic[h] for h in heights[::-1]]
+            
+        
         
     
             
