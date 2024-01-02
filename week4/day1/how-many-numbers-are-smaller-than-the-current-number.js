0ba9b2be532ae3edf969a86ef355b45f5d5e7fc7
Muluken Hailu <108363098+mulukenhailu@users.noreply.github.com>
2024-01-02 11:04:51 -0800

    Added solution for How Many Numbers Are Smaller Than the Current Number

---
+++ week4/day1/how-many-numbers-are-smaller-than-the-current-number.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var smallerNumbersThanCurrent = function(nums) {
+     let counter=0;
+    const arr=[];
+    for(let i=0;i<=nums.length-1;i++){
+        for(let k=0;k<=nums.length-1;k++){
+           if(nums[i]>nums[k]){
+            counter++;
+           }
+       }
+       arr[i]=counter;
+       counter=0;
+    } 
+    return arr;
+    
+};
