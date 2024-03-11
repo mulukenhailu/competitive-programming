c52f4d645de2e2377bf65bc311946084c9b947a5
Muluken Hailu <108363098+mulukenhailu@users.noreply.github.com>
2024-03-11 14:22:20 +0300

    Add solution for N-Queens II

---
+++ week5/leetcode/n-queens-ii.py
@@ -0,0 +1,32 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        board=[["."]*n for _ in range(n)]
+        ans=[]
+
+        col=set()
+        pos=set()
+        neg=set()
+
+        def bt(r):
+            if r >= n:
+                temp=["".join(row) for row in board]
+                ans.append(temp)
+                return 
+
+            for c in range(n):
+                if (c in col) or (r + c in  pos) or (r-c in neg):
+                    continue
+                col.add(c)
+                pos.add(r+c)
+                neg.add(r-c)
+                board[r][c]="Q"
+
+                bt(r+1)
+
+                col.remove(c)
+                pos.remove(r+c)
+                neg.remove(r-c)
+                board[r][c]="."
+
+        bt(0)
+        return len(ans)
